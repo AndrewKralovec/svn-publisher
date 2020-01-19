90cd7dfc48f2fb32d196807a28523db0c68bd0c9
AndrewKralovec <akrala@yahoo.com>
2020-01-19 16:25:18 -0600

    Finish merging script svn commands

--- svn-publisher.sh
+++ svn-publisher.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-##### Args
-# ARG1=${1:-foo}
-# myArray=("$@")
-# for arg in "${myArray[@]}"; do
-#    echo "in $arg"
-# done
-
-##### COMMANDS
-
-# svn info | grep 'Repository Root' | awk '{print $NF}')
-# svn log --stop-on-copy
-# svn merge
-# svn copy <https://subversion.assembla.com/svn/path/to/trunk> \
-#          <https://subversion.assembla.com/svn/path/to/branch_name> \
-#          -m "commit message"
-
+##### Functions
 get_local_repo_url() {
-  local repo_url="$(svn info | grep 'Repository Root' | awk '{print $NF}')"
-  echo "$repo_url"
+  echo "$(svn info | grep 'Repository Root' | awk '{print $NF}')"
+}
+
+get_branch_logs() {
+  echo "$(svn log --stop-on-copy $1/branches/$2)"
+}
+
+merge_branch() {
+  echo "$(svn merge $1/branches/$2)"
 }
 
 ##### Main
-echo "$(svn info | grep 'Repository Root' | awk '{print $NF}')"
-get_local_repo_url
+
+# TODO: make url configurable
+# Get the source branch url
+REPO_BASE_URL="$(get_local_repo_url)"
+# Merge target branch to local repo
+merge_branch $REPO_BASE_URL $1
+# Commit the merge, with the messages from the other branch
+svn commit -m "$(get_branch_logs $REPO_BASE_URL $1)"
